a9bd9181f85bcf70aa834042127d23851e49b6aa
Amitai Burstein <amitai@gizra.com>
2015-05-28 21:10:50 +0300

    Add test and encrypted keys

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -12,20 +12,125 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
    * @Given I am an anonymous user
    */
   public function iAmAnAnonymousUser() {
-    // Sine Jekyll doesn't allow login, we just let this pass-through.
+    // Logout existing user.
+    $this->getSession()->visit($this->locatePath('/#/logout'));
   }
 
   /**
-   * @Then I should the text :text under the main content
+   * @When I visit the homepage
    */
-  public function iShouldTheTextUnderTheMainContent($text) {
-    $this->assertElementContains('.post-content', $text);
+  public function iVisitTheHomepage() {
+    $this->getSession()->visit($this->locatePath('/#/login'));
   }
 
   /**
-   * @Then I should see the author :author
+   * @When I login with my GitHub account
    */
-  public function iShouldSeeTheAuthor($author) {
-    $this->assertElementContains('.author', $author);
+  public function iLoginWithMyGithubAccount() {
+    $this->iWaitForCssElement('.btn-github', 'appear');
+    $element = $this->getSession()->getPage()->find('css', '.btn-github');
+    $element->click();
+
+
+    // We are redirected to GitHub
+    $element = $this->getSession()->getPage();
+
+    if (!$this->getSession()->getPage()->find('css', '#login_field')) {
+      // We are already logged in to GitHub.
+      return;
+    }
+
+
+    $username = getenv('GITHUB_DUMMY_USERNAME');
+    $password = getenv('GITHUB_DUMMY_PASSWORD');
+
+    $element->fillField('login', $username);
+    $element->fillField('password', $password);
+    $element->findButton('commit')->click();
   }
+
+  /**
+   * @Then /^I should wait for the text "([^"]*)" to "([^"]*)"$/
+   */
+  public function iShouldWaitForTheTextTo($text, $appear) {
+    $this->waitForXpathNode(".//*[contains(normalize-space(string(text())), \"$text\")]", $appear == 'appear');
+  }
+
+  /**
+   * @Then /^I wait for css element "([^"]*)" to "([^"]*)"$/
+   */
+  protected function iWaitForCssElement($element, $appear) {
+    $xpath = $this->getSession()->getSelectorsHandler()->selectorToXpath('css', $element);
+    $this->waitForXpathNode($xpath, $appear == 'appear');
+  }
+
+  /**
+   * Wait for an element by its XPath to appear or disappear.
+   *
+   * @param string $xpath
+   *   The XPath string.
+   * @param bool $appear
+   *   Determine if element should appear. Defaults to TRUE.
+   *
+   * @throws Exception
+   */
+  private function waitForXpathNode($xpath, $appear = TRUE) {
+    $this->waitFor(function($context) use ($xpath, $appear) {
+      try {
+        $nodes = $context->getSession()->getDriver()->find($xpath);
+        if (count($nodes) > 0) {
+          $visible = $nodes[0]->isVisible();
+          return $appear ? $visible : !$visible;
+        }
+        return !$appear;
+      }
+      catch (WebDriver\Exception $e) {
+        if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+          return !$appear;
+        }
+        throw $e;
+      }
+    });
+  }
+
+  /**
+   * Helper function; Execute a function until it return TRUE or timeouts.
+   *
+   * @param $fn
+   *   A callable to invoke.
+   * @param int $timeout
+   *   The timeout period. Defaults to 30 seconds.
+   *
+   * @throws Exception
+   */
+  private function waitFor($fn, $timeout = 30000) {
+    $start = microtime(true);
+    $end = $start + $timeout / 1000.0;
+    while (microtime(true) < $end) {
+      if ($fn($this)) {
+        return;
+      }
+    }
+    throw new \Exception('waitFor timed out.');
+  }
+
+  /**
+   * @AfterStep
+   *
+   * Take a screen shot after failed steps for Selenium drivers (e.g.
+   * PhantomJs).
+   */
+  public function takeScreenshotAfterFailedStep($event) {
+    if ($event->getTestResult()->isPassed()) {
+      // Not a failed step.
+      return;
+    }
+    if ($this->getSession()->getDriver() instanceof \Behat\Mink\Driver\Selenium2Driver) {
+      $file_name = sys_get_temp_dir() . DIRECTORY_SEPARATOR . time() . 'behat-failed-step.png';
+      $screenshot = $this->getSession()->getDriver()->getScreenshot();
+      file_put_contents($file_name, $screenshot);
+      print "Screenshot for failed step created in $file_name";
+    }
+  }
+
 }
---
+++ behat/features/github_login.feature
@@ -0,0 +1,14 @@
+Feature: GitHub Login
+  In order to use the system
+  As an anonymous user
+  I need to be able to login using my GitHub account
+
+  @javascript
+  Scenario: Login to shoov
+    Given I am an anonymous user
+     When I visit the homepage
+      And I login with my GitHub account
+     Then I should wait for the text "UI Builds" to "appear"
+
+
+
